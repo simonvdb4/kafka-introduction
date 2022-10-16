10ff61745d8c63d1b2f81c141280d7b7d03239e8
svdenbo1 <simon.vandenbossche@vdab.be>
2022-10-16 11:39:27 +0200

    Initial commit

---
+++ src/main/java/com/consultancy/kafkaintroduction/KafkaIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.consultancy.kafkaintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaIntroductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaIntroductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/consultancy/kafkaintroduction/KafkaIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultancy.kafkaintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaIntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
